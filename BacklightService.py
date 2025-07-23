b9b68aa367edde2ba1579a9836354b27564f46ec
TheFlemoid <trey.dahlberg@gmail.com>
2025-07-22 22:58:23 -0500

    Set a start and end time for when the backlights should be active (hard coded times).  Updated README to specify how to install the package.

--- BacklightService.py
+++ BacklightService.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
 
 import RPi.GPIO as GPIO 
-from time import sleep 
+import time
+import calendar
+import datetime
+
+from time import sleep
+
+earlyTimeString = "07:00:00"
+lateTimeString  = "20:00:00"
+
 GPIO.setwarnings(False) 
  
 GPIO.setmode(GPIO.BCM) 
@@ -10,8 +18,30 @@ GPIO.setup(26, GPIO.OUT)
 
 while True:
     sleep(0.25)
+
+    now = datetime.datetime.now()
+
+    # Converting the early and late time Strings to be the current date
+    earlyTimeNowString = now.strftime("%Y-%m-%d") + " " + earlyTimeString
+    lateTimeNowString = now.strftime("%Y-%m-%d") + " " + lateTimeString
+
+    earlyTime = time.strptime(earlyTimeNowString, "%Y-%m-%d %H:%M:%S")
+    lateTime = time.strptime(lateTimeNowString, "%Y-%m-%d %H:%M:%S")
+
+    earlyDateTime = datetime.datetime(1970, 1, 1) + datetime.timedelta(seconds=calendar.timegm(earlyTime))
+    lateDateTime = datetime.datetime(1970, 1, 1) + datetime.timedelta(seconds=calendar.timegm(lateTime))
+
+    if (now < earlyDateTime or now > lateDateTime):
+        print("LEDs should be active")
+    else:
+        print("LEDs should not be active")
+        GPIO.output(26, False)
+        sleep(5)
+
     if GPIO.input(19) == 1:
         GPIO.output(26, True)
     else:
         GPIO.output(26, False)
+
+    print(now)
 
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Service controlling the backlights on my Raspberry Pi based calendar board.
+
+Place the python script in: `/opt/BacklightService/` and place the service file in `/etc/systemd/system`, then make sure the system is time synced and that the timezone is set correctly, and run `systemctl start BacklightService` and `systemctl enable BacklightService`.
